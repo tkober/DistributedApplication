2a0fb21f2737a9d61f16ffdf04de12ae5f9ce506
Thorsten Kober <Thorsten.Kober@googlemail.com>
2015-10-21 07:40:32 +0200

    Initial Commit

---
+++ AVA/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AVA
+//
+//  Created by Thorsten Kober on 21.10.15.
+//  Copyright © 2015 Thorsten Kober. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AVA/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AVA
+//
+//  Created by Thorsten Kober on 21.10.15.
+//  Copyright © 2015 Thorsten Kober. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
